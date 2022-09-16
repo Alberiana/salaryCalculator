c228ba936539e29e461d227962c41247b3fc9885
Alberiana <atofaj@live.com>
2022-09-16 16:40:25 +0200

    Salary calculator

---
+++ app/src/main/java/com/example/salarycalculator/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.salarycalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText name, surname, pagabr;
+    TextView paga;
+    Button kalkulo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        name=findViewById(R.id.name);
+        surname=findViewById(R.id.surname);
+        pagabr=findViewById(R.id.pagabr);
+        paga=findViewById(R.id.paga);
+        kalkulo=findViewById(R.id.kalkulo);
+        kalkulo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Double pagabr2=Double.parseDouble(pagabr.getText().toString());
+                Double kpm=0.05*pagabr2;
+                Double pagatatueshme=pagabr2-kpm;
+              paga.setText(name.getText().toString()+ " "+surname.getText().toString()+" paga juaj bruto eshte "+ pagabr.getText().toString()+"\n"
+                        + "Nga kjo page, kontributi pensional eshte "+kpm+" ku "+0.05 + "jane nga ju dhe  "+ 0.05 +" jane nga punedhenesi" +"\n"+
+                        "Paga e tatueshme e juaja eshte "+ pagatatueshme
+                );
+           }
+        });
+    }
+}
